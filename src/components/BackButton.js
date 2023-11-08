e82bcd0a8d08fd897eb417108a7261160011554c
Connor-OHara <ConnorSSCS@Gmail.com>
2023-11-08 07:57:15 -0500

    Adding in our Preferences Button. Need work on the icon being cutoff. Good enough for now.

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,27 +1,44 @@
-import React from 'react'
-import { TouchableOpacity, Image, StyleSheet } from 'react-native'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button as PaperButton } from 'react-native-paper';
+import { theme } from '../core/theme';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { getStatusBarHeight } from 'react-native-status-bar-height'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export default function BackButton({ goBack }) {
+export default function PreferencesButton({ mode, goBack, style, onPress, ...props }) {
   return (
-    <TouchableOpacity onPress={goBack} style={GoBackButtonStyles.container}>
-      <Image
-        style={GoBackButtonStyles.image}
-        source={require('../assets/back.png')}
-      />
-    </TouchableOpacity>
-  )
+    <PaperButton
+      style={[
+        PreferenceButtonStyles.button,
+        mode === 'outlined' && { backgroundColor: theme.colors.surface },
+        style,
+      ]}
+      labelStyle={PreferenceButtonStyles.text}
+      mode={mode}
+      onPress={goBack} 
+      {...props}
+    >
+
+      <Icon name="arrow-left" size={30} color="black" style={PreferenceButtonStyles.icon} />
+    </PaperButton>
+  );
 }
 
-export const GoBackButtonStyles = StyleSheet.create({
+export const PreferenceButtonStyles = StyleSheet.create({
   container: {
     position: 'absolute',
     top: 10 + getStatusBarHeight(),
-    left: 4,
-    zIndex: 1
+    left: 10,
+    zIndex: 1,
   },
-  image: {
-    width: 24,
-    height: 24,
+  icon: {
+    marginRight: 10,
   },
-})
+});
+
+
+
+
+
+
