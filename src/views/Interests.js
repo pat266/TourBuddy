cb7b64e0830f37c87b3c796e1c040752deb90616
pat266 <69089199+pat266@users.noreply.github.com>
2023-11-10 20:10:02 -0500

    save preferences

--- src/views/Interests.js
+++ src/views/Interests.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text } from 'react-native-paper';
 import Background from '../components/Background';
@@ -6,11 +6,29 @@ import Header from '../components/Header';
 import Button from '../components/Button';
 import { theme } from '../core/theme';
 import InterestSelection from '../components/InterestSelection';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default function InterestsScreen({ navigation }) {
   const [selectedInterests, setSelectedInterests] = useState([]);
   const [preferredDistance, setPreferredDistance] = useState('');
   const [preferredCost, setPreferredCost] = useState('');
+
+  useEffect(() => {
+    // Save preferences whenever they change
+    savePreferences();
+  }, [selectedInterests, preferredDistance, preferredCost]);
+
+  const savePreferences = async () => {
+    try {
+      await AsyncStorage.setItem('selectedInterests', JSON.stringify(selectedInterests));
+      await AsyncStorage.setItem('preferredDistance', preferredDistance);
+      await AsyncStorage.setItem('preferredCost', preferredCost);
+    } catch (e) {
+      // saving error
+      console.log(e);
+    }
+  };
 
   const onNextPressed = () => {
     // Handle the logic for moving to the next screen here and pass the selected interests
--- src/views/Preferences.js
+++ src/views/Preferences.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text } from 'react-native-paper';
 import Background from '../components/Background';
@@ -6,12 +6,49 @@ import Header from '../components/Header';
 import Button from '../components/Button';
 import { theme } from '../core/theme';
 import InterestSelection from '../components/InterestSelection';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function InterestsScreen({ navigation }) {
   const [selectedInterests, setSelectedInterests] = useState([]);
   const [preferredDistance, setPreferredDistance] = useState('');
   const [preferredCost, setPreferredCost] = useState('');
+
+  useEffect(() => {
+    // Load any saved preferences when the component mounts
+    loadPreferences();
+  }, []);
+
+  useEffect(() => {
+    // Save preferences whenever they change
+    savePreferences();
+  }, [selectedInterests, preferredDistance, preferredCost]);
+
+  const savePreferences = async () => {
+    try {
+      await AsyncStorage.setItem('selectedInterests', JSON.stringify(selectedInterests));
+      await AsyncStorage.setItem('preferredDistance', preferredDistance);
+      await AsyncStorage.setItem('preferredCost', preferredCost);
+    } catch (e) {
+      // saving error
+      console.log(e);
+    }
+  };
+
+  const loadPreferences = async () => {
+    try {
+      const savedInterests = await AsyncStorage.getItem('selectedInterests');
+      const savedDistance = await AsyncStorage.getItem('preferredDistance');
+      const savedCost = await AsyncStorage.getItem('preferredCost');
+
+      if (savedInterests !== null) setSelectedInterests(JSON.parse(savedInterests));
+      if (savedDistance !== null) setPreferredDistance(savedDistance);
+      if (savedCost !== null) setPreferredCost(savedCost);
+    } catch (e) {
+      // loading error
+      console.log(e);
+    }
+  };
 
   const onNextPressed = () => {
     // Handle the logic for moving to the next screen here and pass the selected interests
