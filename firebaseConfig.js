f43bbd61e5e5d1a52ed87b606ac92346bed08d33
Connor-OHara <ConnorSSCS@Gmail.com>
2023-12-01 00:58:32 -0500

    Adding in Firebase Setup Files. Need to modify .env locally

---
+++ firebaseConfig.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+require('dotenv').config();
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'api-key',
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || 'project-id.firebaseapp.com',
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || 'project-id',
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || 'project-id.appspot.com',
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || 'sender-id',
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || 'app-id',
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || 'G-measurement-id',
+};
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+export default firebaseConfig;
